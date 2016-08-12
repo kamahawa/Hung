48b9260a67fff21b8a1493856b41c9a2d06afc30
cuong truong <kamahawa@gmail.com>
2016-08-12 15:56:46 +0700

    thread login

--- SEOGooglePlusSample/SEOGooglePlusSample/FormProgress.cs
+++ SEOGooglePlusSample/SEOGooglePlusSample/FormProgress.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
@@ -16,12 +17,13 @@ namespace SEOGooglePlusSample
     public partial class FormProgress : Form
     {
         string url = "https://plus.google.com";
+        string url_communities = "https://plus.google.com/communities";
 
-        List<string> email, password;
-
-        string search = "music";
-
-        bool flagFirst = true;
+        List<string> email = new List<string>();
+        List<string> password = new List<string>();
+        
+        bool flagComplete = false;
+        int countRun = 0;
 
         //clear session
         private const int INTERNET_OPTION_END_BROWSER_SESSION = 42;
@@ -37,65 +39,111 @@ namespace SEOGooglePlusSample
             string fileName = "account.xml";
             if (File.Exists(fileName))
             {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(fileName);
 
-                FileStream fs = new FileStream(fileName, FileMode.Open);
-                XmlTextReader xtr = new XmlTextReader(fs);
-                while (!xtr.EOF)
+                foreach (XmlNode node in doc.DocumentElement.ChildNodes)
                 {
-                    if (xtr.MoveToContent() == XmlNodeType.Element && xtr.Name == "Username")
-                    {
-                        email.Add(xtr.ReadElementString());
-                    }
-                    else if (xtr.MoveToContent() == XmlNodeType.Element && xtr.Name == "Password")
-                    {
-                        password.Add(xtr.ReadElementString());
-                    }
-                    else
-                    {
-                        xtr.Read();
-                    }
+                    email.Add(node.FirstChild.InnerText);
+                    password.Add(node.LastChild.InnerText);
+
                 }
             }
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
+            webBrowser1.Document.Window.Navigate(url);
+
+            Thread t = new Thread(new ThreadStart(process));
+            t.Start();
+        }
+
+        private void process()
+        {
+            while(true)
+            {
+                if(flagComplete)
+                {
+                    flagComplete = false;
+
+                    if (countRun == 0)
+                    {
+                        inputAccount();
+                    }
+                    else if (countRun == 1)
+                    {
+                        inputPassword();
+                    }
+                    else if (countRun == 2)
+                    {
+                        goToCommunities();
+                    }
+                    Thread.Sleep(1000);
+                }                
+            }
+        }
+
+        void inputAccount()
+        {
+            webBrowser1.Invoke(
+                new MethodInvoker(
+                    delegate 
+                    {
+                        if (webBrowser1.Document.GetElementById("next") != null)
+                        {
+                            webBrowser1.Document.GetElementById("Email").SetAttribute("value", email.ElementAt(0));
+                            webBrowser1.Document.GetElementById("next").InvokeMember("click");
+                        }
+                        else
+                        {
+                            //neu k dang nhap thi hoan tat phuong that dang nhap va chuyen qua buoc khac
+                            flagComplete = true;
+                        }
+                    }
+                )
+            );
+            countRun++;
+        }
+
+        void inputPassword()
+        {
+            webBrowser1.Invoke(
+                new MethodInvoker(
+                    delegate
+                    {
+                        if (webBrowser1.Document.GetElementById("signIn") != null)
+                        {
+                            webBrowser1.Document.GetElementById("Passwd").SetAttribute("value", password.ElementAt(0));
+                            webBrowser1.Document.GetElementById("signIn").InvokeMember("click");
+                        }
+                        else
+                        {
+                            //neu k dang nhap thi hoan tat phuong that dang nhap va chuyen qua buoc khac
+                            flagComplete = true;
+                        }
+                    }
+                )
+            );
+            countRun++;
+        }
+
+        void goToCommunities()
+        {
+            webBrowser1.Invoke(
+                new MethodInvoker(
+                    delegate
+                    {
+                        InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
+                        webBrowser1.Document.Window.Navigate(url_communities);
+                    }
+                )
+            );
+            countRun++;
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            if (flagFirst)
-            {
-                InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
-                webBrowser1.Document.Window.Navigate(url);
-                flagFirst = false;
-            }
-
-            try
-            {
-                if (webBrowser1.Document.GetElementById("next") != null)
-                {
-                    //webBrowser1.Document.GetElementById("Email").SetAttribute("value", email);
-                    //webBrowser1.Document.GetElementById("next").InvokeMember("click");
-                }
-                else if (webBrowser1.Document.GetElementById("signIn") != null)
-                {
-                    //webBrowser1.Document.GetElementById("Passwd").SetAttribute("value", passwd);
-                    //webBrowser1.Document.GetElementById("signIn").InvokeMember("click");
-                }
-                else if (webBrowser1.Document.GetElementById("gbqfq") != null)
-                {
-                    webBrowser1.Document.GetElementById("gbqfq").SetAttribute("value", search);
-                }
-                else
-                {
-                    string text = webBrowser1.Document.Body.ToString();
-                    // WriteAllText creates a file, writes the specified string to the file,
-                    // and then closes the file.    You do NOT need to call Flush() or Close().
-                    System.IO.File.WriteAllText("WriteText.txt", text);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error : " + ex.Message);
-            }
-
+            flagComplete = true;
+            //System.IO.File.WriteAllText("WriteText.txt", webBrowser1.Document.Body.ToString());           
         }
 
         private void FormProgress_FormClosed(object sender, FormClosedEventArgs e)
