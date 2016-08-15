7e2118824c99f0f7f9c5902d167e7e2aec3109c3
cuong.truong <kamahawa@gmail.com>
2016-08-15 23:10:25 +0700

    change

--- SEOGooglePlusSample/SEOGooglePlusSample/FormProgress.cs
+++ SEOGooglePlusSample/SEOGooglePlusSample/FormProgress.cs
@@ -16,14 +16,43 @@ namespace SEOGooglePlusSample
 {
     public partial class FormProgress : Form
     {
+        // link google plus
         string url = "https://plus.google.com";
-        string url_communities = "https://plus.google.com/communities";
 
-        List<string> email = new List<string>();
-        List<string> password = new List<string>();
+        // link cong dong
+        string urlCommunities = "https://plus.google.com/communities";
+
+        // link search
+        string urlSearch = "https://plus.google.com/s/";//https://plus.google.com/s/sach%20hay/communities
+
+        // tu khoa tim kiem cua cong dong
+        string search = "sach hay";
+
+        // so luong thanh vien cua cong dong
+        int numberUser = 50000;
+
+        // list id cong dong
+        List<string> lstCommunities = new List<string>();
         
+        // list email dang nhap
+        List<string> lstEmail = new List<string>();
+
+        // list password
+        List<string> lstPassword = new List<string>();
+        
+        // flag to check load page complete
         bool flagComplete = false;
+
+        // count to step
         int countRun = 0;
+
+        const int INPUT_ACCOUNT = 0;//nhap account
+        const int INPUT_PASSWORD = 1;
+        const int GO_TO_COMMUNITIES = 2;
+        const int GO_TO_SEARCH = 3;
+        const int GO_TO_JOIN = 4;
+
+        const int GO_TO_REST = -1;
 
         //clear session
         private const int INTERNET_OPTION_END_BROWSER_SESSION = 42;
@@ -44,8 +73,8 @@ namespace SEOGooglePlusSample
 
                 foreach (XmlNode node in doc.DocumentElement.ChildNodes)
                 {
-                    email.Add(node.FirstChild.InnerText);
-                    password.Add(node.LastChild.InnerText);
+                    lstEmail.Add(node.FirstChild.InnerText);
+                    lstPassword.Add(node.LastChild.InnerText);
 
                 }
             }
@@ -65,17 +94,25 @@ namespace SEOGooglePlusSample
                 {
                     flagComplete = false;
 
-                    if (countRun == 0)
+                    if (countRun == INPUT_ACCOUNT)
                     {
                         inputAccount();
                     }
-                    else if (countRun == 1)
+                    else if (countRun == INPUT_PASSWORD)
                     {
                         inputPassword();
                     }
-                    else if (countRun == 2)
+                    else if (countRun == GO_TO_COMMUNITIES)
                     {
                         goToCommunities();
+                    }
+                    else if (countRun == GO_TO_SEARCH)
+                    {
+                        goToSearch();
+                    }
+                    else if (countRun == GO_TO_JOIN)
+                    {
+                        goToJoin();
                     }
                     Thread.Sleep(1000);
                 }                
@@ -90,7 +127,7 @@ namespace SEOGooglePlusSample
                     {
                         if (webBrowser1.Document.GetElementById("next") != null)
                         {
-                            webBrowser1.Document.GetElementById("Email").SetAttribute("value", email.ElementAt(0));
+                            webBrowser1.Document.GetElementById("Email").SetAttribute("value", lstEmail.ElementAt(0));
                             webBrowser1.Document.GetElementById("next").InvokeMember("click");
                         }
                         else
@@ -101,7 +138,7 @@ namespace SEOGooglePlusSample
                     }
                 )
             );
-            countRun++;
+            countRun = INPUT_PASSWORD;
         }
 
         void inputPassword()
@@ -112,7 +149,7 @@ namespace SEOGooglePlusSample
                     {
                         if (webBrowser1.Document.GetElementById("signIn") != null)
                         {
-                            webBrowser1.Document.GetElementById("Passwd").SetAttribute("value", password.ElementAt(0));
+                            webBrowser1.Document.GetElementById("Passwd").SetAttribute("value", lstPassword.ElementAt(0));
                             webBrowser1.Document.GetElementById("signIn").InvokeMember("click");
                         }
                         else
@@ -123,7 +160,7 @@ namespace SEOGooglePlusSample
                     }
                 )
             );
-            countRun++;
+            countRun = GO_TO_COMMUNITIES;
         }
 
         void goToCommunities()
@@ -133,11 +170,48 @@ namespace SEOGooglePlusSample
                     delegate
                     {
                         InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
-                        webBrowser1.Document.Window.Navigate(url_communities);
+                        webBrowser1.Document.Window.Navigate(urlCommunities);
                     }
                 )
             );
-            countRun++;
+            countRun = GO_TO_SEARCH;
+        }
+
+        void goToSearch()
+        {
+            webBrowser1.Invoke(
+                new MethodInvoker(
+                    delegate
+                    {
+                        InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
+                        webBrowser1.Document.Window.Navigate(urlSearch + search + "/communities");
+                    }
+                )
+            );
+            countRun = GO_TO_JOIN;
+        }
+
+        void goToJoin()
+        {
+            webBrowser1.Invoke(
+                new MethodInvoker(
+                    delegate
+                    {
+                        var links = webBrowser1.Document.GetElementsByTagName("a");
+                        foreach (HtmlElement link in links)
+                        {
+                            if (link.GetAttribute("className") == "d-s ob b7a n9c Dgc s7c hWd")
+                            {
+                                MessageBox.Show("here");
+                                lstCommunities.Add(link.GetAttribute("data-comm"));
+                            }
+                        }
+                    }
+                )
+            );
+
+            //System.IO.File.WriteAllText("WriteText.txt", webBrowser1.Document.Body.ToString());
+            countRun = GO_TO_REST;
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
